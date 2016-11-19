4663f339ab71264004de68899e504562a1f8dee8
Irene Alvarado <ire.alvarado@gmail.com>
2016-11-19 18:51:47 -0500

    converting from typescript

--- code/vuforia/app.ts
+++ code/vuforia/app.ts
@@ -15,8 +15,8 @@ scene.add(camera);
 scene.add(userLocation);
 
 // We use the standard WebGLRenderer when we only need WebGL-based content
-const renderer = new THREE.WebGLRenderer({ 
-    alpha: true, 
+const renderer = new THREE.WebGLRenderer({
+    alpha: true,
     logarithmicDepthBuffer: true
 });
 // account for the pixel density of the device
@@ -26,8 +26,8 @@ app.view.element.appendChild(renderer.domElement);
 // to easily control stuff on the display
 const hud = new (<any>THREE).CSS3DArgonHUD();
 
-// We put some elements in the index.html, for convenience. 
-// Here, we retrieve the description box and move it to the 
+// We put some elements in the index.html, for convenience.
+// Here, we retrieve the description box and move it to the
 // the CSS3DArgonHUD hudElements[0].  We only put it in the left
 // hud since we'll be hiding it in stereo
 var description = document.getElementById( 'description' );
@@ -40,12 +40,12 @@ hud.hudElements[0].appendChild( stats.dom );
 
 // Tell argon what local coordinate system you want.  The default coordinate
 // frame used by Argon is Cesium's FIXED frame, which is centered at the center
-// of the earth and oriented with the earth's axes.  
+// of the earth and oriented with the earth's axes.
 // The FIXED frame is inconvenient for a number of reasons: the numbers used are
 // large and cause issues with rendering, and the orientation of the user's "local
 // view of the world" is different that the FIXED orientation (my perception of "up"
-// does not correspond to one of the FIXED axes).  
-// Therefore, Argon uses a local coordinate frame that sits on a plane tangent to 
+// does not correspond to one of the FIXED axes).
+// Therefore, Argon uses a local coordinate frame that sits on a plane tangent to
 // the earth near the user's current location.  This frame automatically changes if the
 // user moves more than a few kilometers.
 // The EUS frame cooresponds to the typical 3D computer graphics coordinate frame, so we use
@@ -54,7 +54,7 @@ hud.hudElements[0].appendChild( stats.dom );
 app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
 
 
-// create a bit of animated 3D text that says "argon.js" to display 
+// create a bit of animated 3D text that says "argon.js" to display
 var uniforms = {
     amplitude: { type: "f", value: 0.0 }
 }
@@ -82,7 +82,7 @@ loader.load( '../resources/fonts/helvetiker_bold.typeface.js', function ( font )
     var explodeModifier = new (<any>THREE).ExplodeModifier();
     explodeModifier.modify( textGeometry );
     var numFaces = textGeometry.faces.length;
-    
+
     var bufferGeometry = new THREE.BufferGeometry().fromGeometry( textGeometry );
     var colors = new Float32Array( numFaces * 3 * 3 );
     var displacement = new Float32Array( numFaces * 3 * 3 );
@@ -105,7 +105,7 @@ loader.load( '../resources/fonts/helvetiker_bold.typeface.js', function ( font )
     }
     bufferGeometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
     bufferGeometry.addAttribute( 'displacement', new THREE.BufferAttribute( displacement, 3 ) );
-    
+
     var shaderMaterial = new THREE.ShaderMaterial({
         uniforms: uniforms,
         vertexShader: `
@@ -133,7 +133,7 @@ loader.load( '../resources/fonts/helvetiker_bold.typeface.js', function ( font )
             }
         `
     });
-    
+
     var textMesh = new THREE.Mesh( bufferGeometry, shaderMaterial );
     argonTextObject.add( textMesh );
     argonTextObject.scale.set (0.001,0.001,0.001);
@@ -151,88 +151,84 @@ app.vuforia.isAvailable().then(function(available) {
     if (!available) {
         console.warn("vuforia not available on this platform.");
         return;
-    } 
+    }
 
     // tell argon to initialize vuforia for our app, using our license information.
     app.vuforia.init({
-        encryptedLicenseData: 
+        encryptedLicenseData:
 `-----BEGIN PGP MESSAGE-----
 Version: OpenPGP.js v2.3.2
 Comment: http://openpgpjs.org
 
-wcFMA+gV6pi+O8zeARAAssqSfRHFNoDTNaEdU7i6rVRjht5U4fHnwihcmiOR
-u15f5zQrYlT+g8xDM69uz0r2PlcoD6DWllgFhokkDmm6775Yg9I7YcguUTLF
-V6t+wCp/IgSRl665KXmmHxEd/cXlcL6c9vIFT/heEOgK2hpsPXGfLl1BJKHc
-CqFZ3I3uSCqoM2eDymNSWaiF0Ci6fp5LB7i1oVgB9ujI0b2SSf2NHUa0JfP9
-GPSgveAc2GTysUCqk3dkgcH272Fzf4ldG48EoM48B7e0FLuEqx9V5nHxP3lh
-9VRcAzA3S3LaujA+Kz9/JUOckyL9T/HON/h1iDDmsrScL4PaGWX5EX0yuvBw
-FtWDauLbzAn5BSV+pw7dOmpbSGFAKKUnfhj9d1c5TVeaMkcBhxlkt7j7WvxS
-uuURU3lrH8ytnQqPJzw2YSmxdeHSsAjAWnCRJSaUBlAMj0QsXkPGmMwN8EFS
-9bbkJETuJoVDFfD472iGJi4NJXQ/0Cc4062J5AuYb71QeU8d9nixXlIDXW5U
-fxo9/JpnZRSmWB9R6A2H3+e5dShWDxZF/xVpHNQWi3fQaSKWscQSvUJ83BBP
-ltCvDo+gpD6tTt+3SnAThLuhl38ud7i1B8e0dOCKpuYeSG0rXQPY53n2+mGK
-P1s0e0R7D5jztijwXvGPf45z232cztWsZWvuD2x42DXBwU0DAGn1enGTza0Q
-B/j9y72hJrXx/TdOq85QDMBAA+Ocm9MSGylOqMOb9ozC+DVhhVx7doqS3xV9
-h3jLf6V+OF6VIPHQBxAzH5svlktEOcTtjrjQxnUMmNuHbNQmZlA7uYsAqUpF
-nWqPtJeHMi2F/gYYI/ApK3NGxzJe21dAf2cdp26wf/PoLusotCQH1YVpuR+V
-18Mb8hMpPlB1j5SXnBlv98LxiOGlG6/lQWxpMzkMSZZTxMxa1pCsYNJKK9Bg
-pFUyp4x0W4bQL1mRlqaO04cfoErfHqQzboS2b7WRrNy7YJ9rcBbmpbSc+GEY
-T7ZUPs66EHgdp6uWYPbM1/oajHQBSPALiV65k06XlR4H+QG1ClkSIkbguKnu
-mbpgF7wF5bAfjVVK/ST000Dzr09sgfm4wlIHRcezOzUgjIDVAQE63PznhzfZ
-PEwOKC9ex9t9G+HjvhxICYFoxJLcHJ8ytTWEguNFqSIRTKWTgvAycvTFkJA/
-pasmzov3Nouak8sE28r2NRpWbmI7muLvHfPWgy/rVczF+E1sOkbwtsdOgmym
-yC9yB2IB3fhpLgU28cuI26+cx5IIke0jUgftvza8Oqa0gFZzvu8LaR/RsUdp
-9/CRpiYFvvamNmCDIxxYKtAFCOkEni/5ht4poI2ZxHeWtjwZ2GBqby7BqpUu
-xLXgv+3XpVq1sSUVurKbntDXUy3BwUwDju235GExYfIBEADMsiKpgf0sGKeW
-a5uzMKZgnMm1MoRFBJNsjmBZrbsMxn6lf2ry3XM1xw/w15lepn4X/EMDLeRw
-1m3vw4JL7dLY6e2oOllWyscCs+qE8Cwwx9x6q/gAMfwyrqMQ5EH8psIrRKZM
-eZwGEnSIuUXtJu3ShyqZUqfbpXhr+TxUEXY7n7NuCRJeM70PWPZB5IC1h3Bp
-kgxMRP4zHN2VG4PlcX2fLjpYsx1BHtR2T1biYxbk1AZ26s97XEMH7t9oe+8b
-G+QZc500MmPOd+62UZmnOf/Dul9q/H/0+IlWlWSUTTZFtlL+LwR56t28xqca
-FjUW8TXv6zYUvY7kk5Mlf2iWPA11wJuHaL5DnGaOoNgFVzicNQKy3SfeuYyp
-rSwClM37jRKw+ZNGQDPSAhtrwYZxtndCw/jieqdxIbFG9Td+BunpJNE+KICN
-jmnvG5JrzdueKAyTGqxNOtQnNDJYcg+p5rZVZHGQMN/22n2aiRpWhVAdJIXE
-YgpsFH6R01N3Y55RFNrhusOhuWodj0XuS1EhknU47XyIpNVSZhWG/e+vXMHb
-sN5cO0V7iCFrSxKXg6AwVneoWJC5anT9IabIcgAz07SjdjceC2MlW0vdjPks
-FNygBlP9fTIjBGRzg5QQCh/LyyFUTr1rYRbF+4k5kBQ3MtD2a/lS3Sk1MK/+
-Es9PfWaAoNLB+QGqSi1qtIhds22zelOtc2MGFxgwb/iNZOUccauv6OXThvDD
-gzpn7gZi0+N7pOwx9lJM9QgC4hTMlo268vhNd/MMIPMeyp5n5D8p8ewAutZm
-AcIJkP3h2tUG1V/RvVLF22F+ilh3h++7TeSfHdTdv6ArwDJXdQunHCp3020f
-vhT6XG0ND+UMFtrptJe7+NoRpNg9oZo6kvwDzhPdIa2OlVjXmr25ueC8FlET
-cYdFbIisK+std7/XMlkE5wlGkf9G0RoHsxXqB2Nsj8l3qF5UNyWD+/2Wh+L9
-CDjUbY1FxwlVJ4UZ7lz+8jWHO5jYY99adPoATpUaWYxm9oPxz/QR4kvgvLjl
-9Ti8379Y8qihzqsRmf6YLYyggknlt9Uyl2HjA+1zcwbDnb3I6g/XjTFUPy1D
-xZqqSEuCNDLh7m1+GDA3KXQnLIqOdcxOVzyFCDtKI9c6b0D0ezNkxUjgkoIp
-mxSSLDjzmHuPLsQVwqxP4KNU1gT7mXTnhlhsG2Vll/WZD+tuzGK8h9anf6/p
-4pCk61Dhj1hmb9msTaK4FGhmBMtJ6kQ4SzGOfFKG5IElAHidYgd0iz7AqEzX
-GttDkcHGM9iPIYUBY2r/538M/kxeVx5fBiWEkmWz5FMzqPRs3GZWYiAb2tnp
-WSDXW3B1mwznwcCkyUP6OP/c6FFmb6Rag/ZaItVAvVjmA7tXICLJPhYIs9hE
-I6zJSVZ81YtKg9Nb6Rx49qf18pQ1SWZNGrZrWaTJTLu4cu4c5v/czY5kyT0Y
-8RqNUlI5hwWU8G9LpJ5jv8dssrgcweTG/PEbCkzqz0R6W6VgDUyqo6WSGgoS
-B9or791lGcDazNT6CJ4/2Z1wBd4BSHkhSwfcPovGOleZFE24gLiG6puHyVjk
-WEIir2WXzhypwLkG/dn+ZJW1ezOvTb4gVVILHrWhNh8=
-=LoZg
+wcFMA+gV6pi+O8zeARAAsBbtRPSNVmfFo66jyyzus00VjSuOIJCmD/dQX9Vs
+BU1bqe9SCva0DMifO/xWcgZ6NTtOfVlUMvf6ZmTvCj1QlOJZ9vCICDduSW/b
+/GVH0LRHszJQa4LI3NKZgyHbSjUE8OzDED6m2SUypXtGQ6HmaanWfRyKPyNI
+BX+4k47ejS4s4jaKg3gsphM8y7/ht8qoOm9TXMlW+O0LKhoq8En7O+90udu6
+9UvgemoQR/NaY5JyC/DfYmwuBmMHS2sxJMEC9Zy/0DJXC2J3tVSxjoY4zM3N
+7tcJdU06V4eaVsYxEFfM790moS3xIA9Xg0QD7s5Qt8CTWqDc4lzxQQC+Yjmt
+sahTT3PW+PDOwREssT5pIO4mICUDQDfHfr88R6eSdz2wbznyewXjyCTD8cPE
+Vc4OoP0ws3MJ/KvXQJzrTV9P/Z46aL8UUF8g7ZlTkIC91dVtVnJTsduQa8qP
+RYYOX/qqCDSvV9uhwEwtcHBYeb7HPqKDrpuTFRJbRAHgalWPWJGOg4kg9OWW
+gzHMgluROY2uZP223gjlL/Tn1t0QgxttgAuiuzvTYxuYZ+KTxZWpzsP6fs5g
+OesaE3adhY1aCJHs//If+p5E/C2AmOAe1enkzL5fkg5KjV4A1lkHbZwR81HP
+oSds1U9E2ahc4lNQvAI/ix6fVxUujrkH8R0VgMweXlnBwU4DAGn1enGTza0Q
+B/0YWqDqRpUiyWiUAbUoiU3BYCE4W2CQPRiuBLjaiDPMmJDxxSUnTuV9u43+
+15tD3ibe4j9azavNEddwYBKKabSlKrKRk3hm9D63fFkPGQ0KZ9yy6nTP0qnw
+wF/c7da5vnvDeHyY1DW1Pt5Mm0Tv4v8PFTaguwbSg+tKydzNFwehXEnWms5P
+ToUPuj8qWRgzCwYORuvK+kC26SM0u0SlmKcq0VmqRmuM1zB8BiUWJHqCyhmH
+/FRz3s/gsXXs5qJvZBgIzm/jtbAYWJuHeyJ3X+qM+fO/cDSXGg1SnVzsxZBE
+S/mhA+XpYdb1jue7jx7WcM1L9XGkfcBREtikgnh8kvyVB/9OtKq0LLg7FY0f
+7bFXtNt6ok8dhb3zkZZTVkN2fgcxlU+coutPKhd7hi/1ayXvuMSs4y31KB3K
+DZIMOKjBw5rJfHpH2xlnDMD1QZqsQ9+hvVWNhFq2DevZv4dzquMz2YlwJiBH
+T+JA8hCLPkQ99rm5IJut6TXa9vDEOan/kSXUUrCcKcbAMTReSnkg+AX4xlA8
+i9e9QJeWpRcxxg6GaICafBlJiWEf336g+WV8TnXxMl7eeCC+Sm8ZE8Z1fotC
+vftKNsa7avvhjzzn7K6dS1EU3HMh3KNRS/VXPUeqYTY6TTzr8ZhABwPzqrxQ
+0sj3suDdedd4I5ph0PqxD6qZJ1uWwcFMA47tt+RhMWHyAQ/+N6UPFAVFfIeb
+sVBl366Fm3eZmWZK+2B32aXdcr3VdpcpRiHyYnGqPRJwa8n5RSPqODrXirwo
+68Lf/H55PIBnuAK7uTGgHw8RliD0YpwM6DI1XaX+GHonZ1KN8JNy0UyAtNQZ
+jpfznCh3NeEJHNFBhvjNWCYxKulwMo0MQcgQbIrxLzRMh6j/9Q7Mq8JmNFcY
+tVY9pqHm05D3pHhRo9Hzyj0o7O/cIrSS6hII3ruuEUFFDVNL2Ks7GwP0i5y5
+EuweoBc1d26/4HgtF2gTbbO3Fgv9cFKnItSIvQutsVefLj/rU7fSJLLrpNaB
+GxD70Hvt2JzgP7gZQWLEeOl22ylSDCI0P8N9LAAzPMJdl0zgUrydS78mUYlJ
+EqfZxzskO9J24hw3Bt5LpQgHgSzT7vZVFLW4VucwnDw9K2AOkyPfyFxj+rYn
+1Sl2xzfObPfP8gutGMVrgJ1muFTcOc0qfUrW/euDHiA/li/+lF/RmdrQuwT6
+pXkoM9ewBZVr4OWQ3b0Tyr+KkVF1FD7MhFHLJ1HCxthqvwOyKMsYdkqWaLfZ
++UTa/5a89pJ2xp7uSVYLXmxCflmxY5i6NSiY4somo/2bJ20hYIEI6pgnQ64I
+iurQXSrnUlwd7OdcwnN8+LknvTu4WMAR4odyCbe2HbRru7QrBQWjXMr+UN0c
+AzZNmotGobvSwUQBYvFe9QbvqOFjf1y5QBTHetrGZ/cMLxOiPMrxWH/jSyIo
+cvVY0L8eY0sREFENDHje8aH/zPRXCmmD3la2MNc1eGtRAZ45zq3Yc1j8vTvY
+nj8skepBxwfpe3jGLiTTe6YHtZVZsRb2rBK+W9ZUyzx519MZ24B20DmnKZNC
+5F4cOSHzv8TK9z2xJkqiLkahU0GLg6d782Hw/kFJ3cbcH6tMM4K1XvSabHYY
+CdvS/3q5qZR3jNtOu+fs2Bv0HVg5vGRPu3Kv0uGQwJWQXDlStD6EGqVnz1of
+BRhful876SeBURa3LtQvDMev+yd5vDnxhoHiyMzWt2jS2rZ0696Iy9pHkB/R
+bP6+Z+vRdTeC1SHjOPUP0bzJrtkfRdg7E8KSlVM/62DfUEU6pefZ9gf51yun
+BTO6iWi13Gy6IV3x7KIFIr/0CqwJf9Z7x2MKLAfTu3UVLFYMVGzZP9l5VXZW
+6F/qQ2uUNsj5ek5wL+HLxZdSk2pLmLxdz2v4CimVTzBiZB5MfWD+COevVp5K
+3e8em7mpSyXC/RcaoA0SHjbxX3VRLt3sbWPLHb5CKlCyrNYQQmOPKXa4I7M5
+g1xK9ruZpJU2FkvIp4de03tzAULu51sCvVPouEjNlJMbjyqj47Uc3Cs+xVkf
+btQ2PA8uHJT0igOij/rcj5aX6Z4jVYf+lVYP6GJuK0A=
+=eYMH
 -----END PGP MESSAGE-----`
     }).then((api)=>{
         // the vuforia API is ready, so we can start using it.
 
         // tell argon to download a vuforia dataset.  The .xml and .dat file must be together
-        // in the web directory, even though we just provide the .xml file url here 
-        api.objectTracker.createDataSet("../resources/datasets/ArgonTutorial.xml").then( (dataSet)=>{
+        // in the web directory, even though we just provide the .xml file url here
+        api.objectTracker.createDataSet("../resources/datasets/IxDMusic.xml").then( (dataSet)=>{
             // the data set has been succesfully downloaded
 
-            // tell vuforia to load the dataset.  
+            // tell vuforia to load the dataset.
             dataSet.load().then(()=>{
                 // when it is loaded, we retrieve a list of trackables defined in the
                 // dataset and set up the content for the target
                 const trackables = dataSet.getTrackables();
 
                 // tell argon we want to track a specific trackable.  Each trackable
-                // has a Cesium entity associated with it, and is expressed in a 
+                // has a Cesium entity associated with it, and is expressed in a
                 // coordinate frame relative to the camera.  Because they are Cesium
                 // entities, we can ask for their pose in any coordinate frame we know
                 // about.
-                const gvuBrochureEntity = app.context.subscribeToEntityById(trackables["GVUBrochure"].id)
+                const gvuBrochureEntity = app.context.subscribeToEntityById(trackables["bluestone"].id)
 
                 // create a THREE object to put on the trackable
                 const gvuBrochureObject = new THREE.Object3D;
@@ -252,10 +248,10 @@ WEIir2WXzhypwLkG/dn+ZJW1ezOvTb4gVVILHrWhNh8=
                         gvuBrochureObject.quaternion.copy(<any>gvuBrochurePose.orientation);
                     }
 
-                    // when the target is first seen after not being seen, the 
+                    // when the target is first seen after not being seen, the
                     // status is FOUND.  Here, we move the 3D text object from the
                     // world to the target.
-                    // when the target is first lost after being seen, the status 
+                    // when the target is first lost after being seen, the status
                     // is LOST.  Here, we move the 3D text object back to the world
                     if (gvuBrochurePose.poseStatus & Argon.PoseStatus.FOUND) {
                         gvuBrochureObject.add(argonTextObject);
@@ -268,7 +264,7 @@ WEIir2WXzhypwLkG/dn+ZJW1ezOvTb4gVVILHrWhNh8=
             }).catch(function(err) {
                 console.log("could not load dataset: " + err.message);
             });
-            
+
             // activate the dataset.
             api.objectTracker.activateDataSet(dataSet);
         });
@@ -285,18 +281,18 @@ app.context.updateEvent.addEventListener(() => {
     // in the local coordinate frame.
     const userPose = app.context.getEntityPose(app.context.user);
 
-    // assuming we know the user's pose, set the position of our 
+    // assuming we know the user's pose, set the position of our
     // THREE user object to match it
     if (userPose.poseStatus & Argon.PoseStatus.KNOWN) {
         userLocation.position.copy(<any>userPose.position);
     }
 });
-    
+
 // renderEvent is fired whenever argon wants the app to update its display
 app.renderEvent.addEventListener(() => {
     // update the rendering stats
     stats.update();
-    
+
     // if we have 1 subView, we're in mono mode.  If more, stereo.
     var monoMode = (app.view.getSubviews()).length == 1;
 
@@ -306,15 +302,15 @@ app.renderEvent.addEventListener(() => {
     const viewport = app.view.getViewport();
     renderer.setSize(viewport.width, viewport.height);
     hud.setSize(viewport.width, viewport.height);
-    
-    // there is 1 subview in monocular mode, 2 in stereo mode    
+
+    // there is 1 subview in monocular mode, 2 in stereo mode
     for (let subview of app.view.getSubviews()) {
-        // set the position and orientation of the camera for 
+        // set the position and orientation of the camera for
         // this subview
         camera.position.copy(<any>subview.pose.position);
         camera.quaternion.copy(<any>subview.pose.orientation);
         // the underlying system provide a full projection matrix
-        // for the camera. 
+        // for the camera.
         camera.projectionMatrix.fromArray(<any>subview.projectionMatrix);
 
         // set the viewport for this view
