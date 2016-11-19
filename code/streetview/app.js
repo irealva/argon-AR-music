88c934426ed59d7623b5aedef4fa0731d7c48752
Irene Alvarado <ire.alvarado@gmail.com>
2016-11-19 14:02:59 -0500

    removing other examples

--- code/streetview/app.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="@argonjs/argon" />
-// set up Argon
-var app = Argon.init();
-// set our desired reality 
-app.reality.setDesired({
-    title: 'My Nearest Streetview',
-    uri: Argon.resolveURL('../streetview-reality/index.html')
-});
