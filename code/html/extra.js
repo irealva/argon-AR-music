bb55b04116bce1d279ff98a0422eb83dac6ee31d
Irene Alvarado <ire.alvarado@gmail.com>
2016-12-02 15:39:08 -0500

    Adding a waveform in middle of screen

---
+++ code/html/extra.js
@@ -0,0 +1,22 @@
+var ctx = document.createElement('canvas').getContext('2d');
+    var linGrad = ctx.createLinearGradient(0, 64, 0, 200);
+    linGrad.addColorStop(0.5, 'rgba(255, 255, 255, 1.000)');
+    linGrad.addColorStop(0.5, 'rgba(183, 183, 183, 1.000)');
+
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: linGrad,
+      progressColor: 'hsla(200, 100%, 30%, 0.5)',
+      cursorColor: '#fff',
+      // This parameter makes the waveform look like SoundCloud's player
+      barWidth: 3
+});
+
+wavesurfer.load('song2.mp3');
+
+// Regions
+    if (wavesurfer.enableDragSelection) {
+        wavesurfer.enableDragSelection({
+            color: 'rgba(0, 255, 0, 0.1)'
+        });
+    }
